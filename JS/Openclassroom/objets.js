24d4050c7dab53bfb41d9395a33cde85134fabd0
Brown <brown@MacBook-Pro-de-Brown.local>
2020-03-23 15:42:32 +0100

    next

---
+++ JS/Openclassroom/objets.js
@@ -0,0 +1,9 @@
+let myBook = {
+    title: "L'Histoire de Tao",
+    author: "Will Alexander",
+    numberOfPages: 250,
+    isAvailable: true
+};
+
+let bookTitle = myBook.title;  // "L'Histoire de Tao"
+let bookPages = myBook.numberOfPages  // 250
